e5ece3869963b789797696cb8d1d6453fa9c6af3
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2023-06-06 23:26:30 -0500

    Create Timely.java

---
+++ CodeChef/Reach on Time/Timely.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in = new Scanner(System.in);
+		
+		int t = in.nextInt();
+		
+		for( int i = 0; i < t; i++){
+		    int x = in.nextInt();
+		    
+		    if(x < 30){
+		        System.out.println("NO");
+		    }
+		    else{
+		        System.out.println("YES");
+		    }
+		}
+	}
+}
