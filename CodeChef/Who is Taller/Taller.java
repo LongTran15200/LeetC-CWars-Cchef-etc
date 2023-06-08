67bab0b427fa2b25dc204d6688241d2295f633d7
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2023-06-07 23:19:38 -0500

    Create Taller.java

---
+++ CodeChef/Who is Taller/Taller.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in = new Scanner(System.in);
+		
+		int t = in.nextInt();
+		
+		for(int i = 0; i < t; i++){
+		    int x = in.nextInt();
+		    int y = in.nextInt();
+		    
+		    if(x < y){
+		        System.out.println("B");
+		        
+		    }
+		    else{
+		        System.out.println("A");
+		    }
+		}
+	}
+}
