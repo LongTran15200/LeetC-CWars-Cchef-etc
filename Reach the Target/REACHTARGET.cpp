0ead442dc89ea9097299e697900cc74007bbb526
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2023-06-08 23:24:21 -0500

    Create REACHTARGET.cpp

---
+++ Reach the Target/REACHTARGET.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,y,x;
+	
+	cin >> t;
+	
+	while(t--){
+	    
+	    cin >> y >> x;
+	    
+	    cout << y-x << endl;
+	}
+	return 0;
+}
