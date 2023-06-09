ad4d008e8054db9f12140232babe150dc0ec98db
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2023-06-09 01:04:17 -0500

    Create TODOLIST.cpp

---
+++ CodeChef/Problems in your to-do list DR:580/TODOLIST.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,n,d;
+	cin >> t;
+	
+	while(t--){
+	    
+	    cin >> n;
+	    int count = 0;
+	    int arr[n];
+	    
+	        for(int i = 0; i < n; i++){
+	            cin >> arr[i];
+	           	
+	           	if(arr[i] >= 1000){
+	           	    count++;
+	           	}
+	            }
+	       
+	   cout << count << endl;
+	}
+	return 0;
+}
