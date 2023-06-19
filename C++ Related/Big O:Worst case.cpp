4174808ca5c88b7a16a2dc12a6103957a328bbac
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2023-06-18 22:59:08 -0500

    Create Big O:Worst case.cpp

---
+++ C++ Related/Big O:Worst case.cpp
@@ -0,0 +1,15 @@
+/*Lets give an example of an array[]= {1,2,3,4,5};
+
+1 2 3 4 5 
+^   ^   ^
+|   |   |
+Ω   θ   O
+
+Ω = Best case
+θ = Average case
+O = Worst case
+
+You won't hear people talk about Best/Average case as much as Worst case.
+Big O will always be worst case.
+
+*/
