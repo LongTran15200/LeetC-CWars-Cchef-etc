3bf01adc7023678dbd526a683f5d96cb96c5e568
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2023-06-22 01:27:42 -0500

    altERnaTIngcAsE.cpp

--- Codewars/kyu8/altERnaTIngcAsE.cpp
+++ Codewars/kyu8/altERnaTIngcAsE.cpp
@@ -13,3 +13,28 @@ std::string toAlternatingCase(const std::string& str) {
 
     return result;
 }
+
+
+/*
+Adapted from the tests originally written by a code warrior donaldsebleung */
+Describe(basic)
+{
+  It(ShouldPassAllTheTestsProvided)
+  {
+    
+       Assert::That(to_alternating_case("hello world"), Equals("HELLO WORLD") );
+       Assert::That(to_alternating_case("HELLO WORLD"), Equals("hello world") );
+       Assert::That(to_alternating_case("hello WORLD"), Equals("HELLO world") );
+       Assert::That(to_alternating_case("HeLLo WoRLD"), Equals("hEllO wOrld") );
+       Assert::That(to_alternating_case("12345"), Equals("12345") );
+       Assert::That(to_alternating_case("1a2b3c4d5e"), Equals("1A2B3C4D5E") );
+       Assert::That(to_alternating_case("String.prototype.toAlternatingCase"), Equals("sTRING.PROTOTYPE.TOaLTERNATINGcASE") );
+       Assert::That(to_alternating_case(to_alternating_case("Hello World")), Equals("Hello World"));
+		   Assert::That(to_alternating_case("altERnaTIng cAsE"), Equals("ALTerNAtiNG CaSe") );
+		   Assert::That(to_alternating_case("ALTerNAtiNG CaSe"), Equals("altERnaTIng cAsE") );
+		   Assert::That(to_alternating_case("altERnaTIng cAsE <=> ALTerNAtiNG CaSe"), Equals("ALTerNAtiNG CaSe <=> altERnaTIng cAsE") );
+		   Assert::That(to_alternating_case("ALTerNAtiNG CaSe <=> altERnaTIng cAsE"), Equals("altERnaTIng cAsE <=> ALTerNAtiNG CaSe") );
+  }
+
+};
+*/
