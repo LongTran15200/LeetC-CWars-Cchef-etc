903b7e1f79967fb7925db8032a34293b6eceb31e
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2023-08-04 20:28:30 -0500

    Create LinkedList.cpp

---
+++ LeetCode/LinkedList/LinkedList.cpp
@@ -0,0 +1,107 @@
+#include <iostream> 
+#include "LinkedList.h"
+
+using namespace std;
+
+
+Node::Node(int value) {
+    this->value = value;
+    next = nullptr;
+}
+
+LinkedList::LinkedList(int value) {
+    Node* newNode = new Node(value);
+    head = newNode;
+    tail = newNode;
+    length = 1;
+}
+
+/*
+LinkedList::~LinkedList() {
+    Node* temp = head;
+    while (head) {
+        head = head->next;
+        delete temp;
+        temp = head;
+    }
+}
+
+void LinkedList::printList() {
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->value << endl;
+        temp = temp->next;
+    }
+}
+
+void LinkedList::printAll() {
+    if (length == 0) {
+        cout << "Head: null" << endl;
+        cout << "Tail: null" << endl;
+    } else {
+        cout << "Head: " << head->value << endl;
+        cout << "Tail: " << tail->value << endl;
+    }
+    cout << "Length: " << length << endl;
+    cout << "\nLinked List:" << endl;
+    if (length == 0) {
+        cout << "empty" << endl;
+    } else {
+        printList();
+    }
+}
+
+Node* LinkedList::getHead() {
+    return head;
+}
+
+Node* LinkedList::getTail() {
+    return tail;
+}
+
+int LinkedList::getLength() {
+    return length;
+}
+*/
+  
+void LinkedList::append(int value) {
+    Node* newNode = new Node(value);
+    if (length == 0) {
+        head = newNode;
+        tail = newNode;
+    } else {
+        tail->next = newNode;
+        tail = newNode;
+    }
+    length++;
+}
+
+//You start at the beginning of the linkedlist and start iterate down to the last element of the linkedlist
+void LinkedList::deleteLast() {
+    //if no variable inside linkedlist, return.
+    if(length == 0){
+        return;
+    }
+    Node* temp = head;
+    Node* pre  = head;
+
+  //While temp is pointing to next that is not nullptr
+    while(temp->next){
+      
+        pre = temp;
+        temp = temp->next;
+    }
+    tail = pre;
+    tail->next = nullptr;
+    length--;
+  
+    if(length == 0){
+        tail = nullptr;
+        head = nullptr;
+    }
+  //delete temp from the memmory.
+    delete temp;
+}
+
+
+
