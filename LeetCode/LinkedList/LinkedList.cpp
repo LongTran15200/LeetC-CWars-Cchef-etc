074be643dc999f7d9846bf652d822d3996aabc4e
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2023-08-08 22:54:27 -0500

    Update LinkedList.cpp

--- LeetCode/LinkedList/LinkedList.cpp
+++ LeetCode/LinkedList/LinkedList.cpp
@@ -102,6 +102,96 @@ void LinkedList::deleteLast() {
   //delete temp from the memmory.
     delete temp;
 }
+void LinkedList::prepend(int value) {
+    Node* newNode = new Node(value);
+    if (length == 0) {
+        head = newNode;
+        tail = newNode;
+    } else {
+        newNode->next = head;
+        head = newNode;
+    }
+    length++;
+}
+
+void LinkedList::deleteFirst() {
+    if (length == 0) return;
+    Node* temp = head;
+    if (length == 1) {
+        head = nullptr;
+        tail = nullptr;
+    } else {
+        head = head->next;
+    }
+    delete temp;
+    length--;
+}
+
+Node* LinkedList::get(int index) {
+    if (index < 0 || index >= length) return nullptr;
+    Node* temp = head;
+    for (int i = 0; i < index; ++i) {
+        temp = temp->next;
+    }
+    return temp;
+}
+
+bool LinkedList::set(int index, int value) {
+    Node* temp = get(index);
+    if (temp) {
+        temp->value = value;
+        return true;
+    } 
+    return false;
+}
+
+bool LinkedList::insert(int index, int value) {
+    if (index < 0 || index > length) return false;
+    if (index == 0) {
+        prepend(value);
+        return true;
+    }
+    if (index == length) {
+        append(value);
+        return true;
+    }
+    Node* newNode = new Node(value);
+    Node* temp = get(index - 1);
+    newNode->next = temp->next;
+    temp->next = newNode;
+    length++;
+    return true;
+}
+
+void LinkedList::deleteNode(int index) {
+    if (index < 0 || index >= length) return;
+    if (index == 0) return deleteFirst();
+    if (index == length - 1) return deleteLast();
+
+    Node* prev = get(index - 1);
+    Node* temp = prev->next;
+
+    prev->next = temp->next;
+    delete temp;
+    length--;        
+}
+
+void LinkedList::reverse() {
+    Node* temp = head;
+    head = tail;
+    tail = temp;
+    
+    Node* before = nullptr;
+    Node* after = temp->next;
+    
+    for(int i = 0; i < length; i++){
+        after = temp->next;
+        temp->next = before;
+        before = temp;
+        temp = after;
+    }
+}
+
 
 
 
