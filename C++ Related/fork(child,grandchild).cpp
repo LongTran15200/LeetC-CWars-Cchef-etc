12c997d3f2424753682488c1c848f49d0d058316
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2023-10-11 23:53:19 -0500

    Create fork(child,grandchild).cpp

---
+++ C++ Related/fork(child,grandchild).cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+  pid_t pid;
+  std::cout << "I am the parent process" << std::endl;
+  
+  for(int i = 0; i < 4; i++){
+      
+      pid = fork();
+      
+      if(pid == 0){
+          
+         std::cout << "I am the child process " << i << std::endl;
+         
+         if(i == 1){
+            for(int j = 0; j < 2; j++){
+                
+               pid = fork();
+               
+               if(pid == 0){
+                 std::cout << "I am a grandchild process from child process " << i << std::endl;
+                
+                 _exit(0);
+               }
+               wait(nullptr);
+            }
+         }
+        _exit(0);
+      }
+      wait(nullptr);
+  }
+  
+    return 0;
+}
+
+/*
+Output
+I am the parent process
+I am the child process 0
+I am the child process 1
+I am a grandchild process from child process 1
+I am a grandchild process from child process 1
+I am the child process 2
+I am the child process 3
+*/
