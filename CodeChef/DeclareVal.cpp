86e6898fa1364f0356239be87f9a9bbbbb417b4c
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2024-07-15 07:18:57 -0500

    Update DeclareVal.cpp

--- CodeChef/DeclareVal.cpp
+++ CodeChef/DeclareVal.cpp
@@ -9,3 +9,20 @@ int main() {
     cout << number ;
 }
 //calm down dont laugh, I havent code in 7 months. Regaining my c++ lmao.
+
+
+//Addition val
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Assign the value 23 to a
+    int a = 23;
+    
+    // Assign the value 20 to b
+    int b = 20;
+    
+    // Output the sum of a and b
+    cout << a+b << endl;
+    
+}
