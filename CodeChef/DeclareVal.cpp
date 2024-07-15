7bc55e208926a7f32e1718015d10d15239ff9158
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2024-07-15 07:16:33 -0500

    Create DeclareVal.cpp

---
+++ CodeChef/DeclareVal.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare a variable
+    int number = 19;
+    
+    // Print the variable
+    cout << number ;
+}
+//calm down dont laugh, I havent code in 7 months. Regaining my c++ lmao.
