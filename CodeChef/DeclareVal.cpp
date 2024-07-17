79d057b8931a73547a8fe9f3ff1e57f1b591f1c3
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2024-07-17 09:45:26 -0500

    Update DeclareVal.cpp

--- CodeChef/DeclareVal.cpp
+++ CodeChef/DeclareVal.cpp
@@ -40,3 +40,15 @@ int main() {
 
   
 }
+//Concatenation
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main () {
+    string x = "Hello";
+    string y = "World";
+    cout<< x + " " + y;
+  
+}
+
+
