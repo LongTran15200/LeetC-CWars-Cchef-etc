5881684953ca3b6e83b0df795a140749a3f551c1
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2024-07-18 23:36:38 -0500

    Update TLG.cpp

--- CodeChef/The Lead Game/TLG.cpp
+++ CodeChef/The Lead Game/TLG.cpp
@@ -1,34 +1,37 @@
-#include <stdio.h>
+#include <iostream>
 
-int main()
-{
-    int t,i,m=0,n=0,d1=0,d2=0,lead1=0,lead2=0,p1,p2;
-    scanf("%d",&t);
-    for(i=0;i<t;i++)
-    {
-        scanf("%d%d",&p1,&p2);
-        m=m+p1;
-        n=n+p2;
-        if (m>=n)
-        {
-	        d1=m-n;
+int main() {
+    int t;
+    std::cin >> t;
+
+    int m = 0, n = 0, d1 = 0, d2 = 0, lead1 = 0, lead2 = 0;
+    int p1, p2;
+
+    for (int i = 0; i < t; ++i) {
+        std::cin >> p1 >> p2;
+        m += p1;
+        n += p2;
+
+        if (m >= n) {
+            d1 = m - n;
+        } else {
+            d2 = n - m;
         }
-        if (m<n)
-        {
-            d2=n-m;
+
+        if (d1 > lead1) {
+            lead1 = d1;
         }
-        if (d1>lead1)
-        {
-            lead1=d1;
-        }
-        if (d2>lead2)
-        {
-            lead2=d2;
+
+        if (d2 > lead2) {
+            lead2 = d2;
         }
     }
-    if (lead1>lead2)
-	    printf ("1 %d",lead1);
-    if (lead2>lead1)
-	    printf ("2 %d",lead2);
-return 0;
+
+    if (lead1 > lead2) {
+        std::cout << "1 " << lead1 << std::endl;
+    } else {
+        std::cout << "2 " << lead2 << std::endl;
+    }
+
+    return 0;
 }
