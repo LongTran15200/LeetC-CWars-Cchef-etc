cda398f8ae621b359022224be74f35bd0068abc0
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2024-07-21 12:13:46 -0500

    Update charReport.py

--- boot.dev/python/charReport.py
+++ boot.dev/python/charReport.py
@@ -22,3 +22,32 @@ print("Character Report Complete")
 print(
     f"Data types {type(level)}, {type(name)}, {type(character_class)}, {type(account_active)}, {type(pvp_rank)}, {type(max_health)}, {type(max_mana)}, {type(armor)}, {type(magic_resistance)}"
 )
+
+
+
+
+#dif
+level = 25
+name = "Lopen"
+character_class = "Windrunner"
+account_active = True
+pvp_rank = "Squire"
+max_health = 79
+max_mana = 274
+armor = 12
+magic_resistance = 15.4
+
+print("Character Report")
+print(
+    f"Character {name} is a level {level} {character_class}. And are currently ranked as a {pvp_rank}."
+)
+print(f"They have {max_health} max health and {max_mana} max mana.")
+print(f"They have {armor} armor and {magic_resistance} magic resistance.")
+print(f"Their account is currently active: {account_active}")
+
+# Don't edit below this line
+
+print("Character Report Complete")
+print(
+    f"Data types {type(level)}, {type(name)}, {type(character_class)}, {type(account_active)}, {type(pvp_rank)}, {type(max_health)}, {type(max_mana)}, {type(armor)}, {type(magic_resistance)}"
+)
