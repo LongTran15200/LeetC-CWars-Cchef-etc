9190506ddcdf1433a956da4ecb85efda389bd551
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2024-07-21 12:17:43 -0500

    Create function.py

---
+++ boot.dev/python/function.py
@@ -0,0 +1,21 @@
+def area_of_circle(radius):
+    pi = 3.14
+    area = pi * radius * radius
+    return area
+
+
+sword_length = 1.0
+spear_length = 2.0
+
+# don't touch above this line
+
+sword_area = area_of_circle(sword_length)
+spear_area = area_of_circle(spear_length)
+
+# don't touch below this line
+
+print("Sword length:", sword_length, "meters.")
+print("Sword attack area:", sword_area, "square meters")
+
+print("Spear length:", spear_length, "meters.")
+print("Spear attack area:", spear_area, "square meters")
