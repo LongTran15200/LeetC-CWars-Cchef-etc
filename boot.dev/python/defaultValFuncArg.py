bbff77702abd81631f7f91988aed7093fe647cc4
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2024-07-23 16:54:03 -0500

    Create defaultValFuncArg.py

---
+++ boot.dev/python/defaultValFuncArg.py
@@ -0,0 +1,31 @@
+def get_punched(health, armor = 0):
+    # ?
+    finalHealth = health + armor - 50
+    return finalHealth
+
+def get_slashed(health, armor = 0):
+    # ?
+    finalHealth = health + armor - 100
+    return finalHealth
+
+# Don't touch below this line
+
+
+def test(health, armor):
+    print(f"Health: {health}, Armor: {armor}")
+    print(f"Health after punch: {get_punched(health, armor)}")
+    print("=====================================")
+    print(f"Health: {health}, Armor: {armor}")
+    print(f"Health after slash: {get_slashed(health, armor)}\n")
+    print("=====================================")
+    print(f"Health: {health}, Armor: no armor!")
+    print(f"Health after slash: {get_slashed(health)}\n")
+    print("=====================================")
+    print(f"Health: {health}, Armor: no armor!")
+    print(f"Health after punch: {get_punched(health)}")
+    print("=====================================")
+
+
+test(400, 5)
+test(300, 3)
+test(200, 1)
