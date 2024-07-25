df992000ab2943ef1e1d5d86c6f87f1f205f3311
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2024-07-25 03:07:57 -0500

    Create idk.py

---
+++ boot.dev/python/idk.py
@@ -0,0 +1,25 @@
+def curse(weapon_damage):
+    lesser_cursed = weapon_damage * 0.5
+    greater_cursed = weapon_damage * 0.25
+    return lesser_cursed, greater_cursed
+
+
+# Don't modify below this line
+
+
+def test(weapon_damage):
+    print("Weapon's base damage:", float(weapon_damage))
+    print("Cursing...")
+    lesser_cursed, greater_cursed = curse(weapon_damage)
+    print("With lesser curse the damage is:", float(lesser_cursed), "damage.")
+    print("With greater curse the damage is:", float(greater_cursed), "damage.")
+    print("=====================================")
+
+
+def main():
+    test(100)
+    test(500)
+    test(1000)
+
+
+main()
