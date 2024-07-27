e657379e256e75f2cc6d492db48703b97657e949
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2024-07-26 20:33:11 -0500

    Update idk.py

--- boot.dev/python/idk.py
+++ boot.dev/python/idk.py
@@ -20,3 +20,35 @@ def main():
 
 
 main()
+
+
+
+
+#attack and enchant
+def enchant_and_attack(target_health, damage, weapon):
+    # ?
+    enchanted_damage = damage + 10
+    new_health = target_health - enchanted_damage
+    enchanted_weapon = "enchanted " + weapon
+    return enchanted_weapon, new_health
+
+
+# Don't modify below this line
+
+
+def test(target_health, damage, weapon):
+    print("The target has", target_health, "health.")
+    print(weapon, "base damage:", damage, "Enchanting and attacking")
+    enchanted_weapon, new_health = enchant_and_attack(target_health, damage, weapon)
+    print("The target has been attacked with the", enchanted_weapon)
+    print("The target has", new_health, "health remaining.")
+    print("=====================================")
+
+
+def main():
+    test(100, 50, "sword")
+    test(500, 100, "axe")
+    test(1000, 250, "bow")
+
+
+main()
