c8243ff79dacdde648f388937b2022d73c5c87d3
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2024-12-05 00:17:48 -0600

    Update BitCounting.py

--- Codewars/kyu6/BitCounting.py
+++ Codewars/kyu6/BitCounting.py
@@ -1,7 +1,11 @@
 def count_bits(n):
     return bin(n).count('1')
 
-# Example usage
 number = 1234
 result = count_bits(number)
 print(f"The number of 1 bits in the binary representation of {number} is {result}.")
+
+
+
+#output
+#The number of 1 bits in the binary representation of 1234 is 5. //1234 is 10011010010 in bits. In total, there are 5 1s.
