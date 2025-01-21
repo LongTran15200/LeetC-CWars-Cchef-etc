d9ec6a91a3068036499b94d6952a796d7e3b72d4
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2025-01-20 18:45:46 -0600

    Update Dice.py

--- Python/Dice.py
+++ Python/Dice.py
@@ -1,4 +1,6 @@
-dice_art ={1:  ("┌─────────┐",
+import random
+
+dice_art ={ 1: ("┌─────────┐",
                 "│         │",
                 "│    ●    │",
                 "│         │",
@@ -27,26 +29,64 @@ dice_art ={1:  ("┌─────────┐",
                 "│  ●   ●  │",
                 "│  ●   ●  │",
                 "│  ●   ●  │",
-                "└─────────┘")               
+                "└─────────┘"),               
+            7: ("┌─────────┐",
+                "│  ●   ●  │",
+                "│  ● ● ●  │",
+                "│  ●   ●  │",
+                "└─────────┘"),
+            8: ("┌─────────┐",
+                "│  ● ● ●  │",
+                "│  ●   ●  │",
+                "│  ● ● ●  │",
+                "└─────────┘"),
+            9: ("┌─────────┐",
+                "│  ● ● ●  │",
+                "│  ● ● ●  │",
+                "│  ● ● ●  │",
+                "└─────────┘"),                
+            10:("┌─────────┐",
+                "│ ● ● ● ● │",
+                "│ ●     ● │",
+                "│ ● ● ● ● │",
+                "└─────────┘"),
+            11:("┌─────────┐",
+                "│ ● ● ● ● │",
+                "│ ●  ●  ● │",
+                "│ ● ● ● ● │",
+                "└─────────┘"),
+            12:("┌─────────┐",
+                "│ ● ● ● ● │",
+                "│ ● ● ● ● │",
+                "│ ● ● ● ● │",
+                "└─────────┘")
 }
 dice = []
 total = 0
-num_of_dice = int(input("How many dice?:"))
+
+num_of_dice = int(input("Enter number of dice: "))
 
 for die in range(num_of_dice):
-    dice.append(random.randint(1,6))
+    dice.append(random.randint(1,12))
 
-# This output horizontal dice
-# for die in dice:
-#     for line in dice_art[die]:
-#         print(line)
+print(dice)
 
+odd_dice = [die for die in dice if die%2 != 0]
+even_dice = [die for die in dice if die%2 == 0]
 
-# this will output veritcal dice
-for line in range(5):
-    for die in dice:
-        print(dice_art.get(die)[line], end="")
-    print()
+if odd_dice:
+    for i in range(0,len(odd_dice),2):
+        for line in range(5):
+            for die in odd_dice[i:i+2]:
+                print(dice_art[die][line], end="")
+            print()
+
+if even_dice:
+    for i in range(0,len(even_dice),2):
+        for line in range(5):
+            for die in even_dice[i:i+2]:
+                print(dice_art[die][line], end="")
+            print()
 
 
 total = sum(dice)
