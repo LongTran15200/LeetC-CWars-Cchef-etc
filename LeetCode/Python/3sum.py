58b8ac4853e7edfaab5b6020553b96deef8f6832
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2025-04-18 09:16:40 -0500

    Create 3sum.py

---
+++ LeetCode/Python/3sum.py
@@ -0,0 +1,40 @@
+# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+# Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+# Example 1:
+
+# Input: nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+# Explanation: 
+# nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+# nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+# nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+# The distinct triplets are [-1,0,1] and [-1,-1,2].
+# Notice that the order of the output and the order of the triplets does not matter.
+# Example 2:
+
+# Input: nums = [0,1,1]
+# Output: []
+# Explanation: The only possible triplet does not sum up to 0.
+# Example 3:
+
+# Input: nums = [0,0,0]
+# Output: [[0,0,0]]
+# Explanation: The only possible triplet sums up to 0.
+
+#Time complexity O(n^3)
+class Solution(object):
+    def threeSum(self, nums):
+        result = []
+        nums.sort()  #sort in order to prevent any duplication 
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue  # continue will help move iterate to next if duplicate number. similar to line 12 and 16
+
+            for j in range(i + 1, len(nums)):
+                if j > i + 1 and nums[j] == nums[j-1]:
+                    continue
