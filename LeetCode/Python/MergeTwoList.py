51ca5903aea3ff11ab7f3e95ccf072441e99d5c4
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2025-05-01 21:07:50 -0500

    Create MergeTwoList.py

---
+++ LeetCode/Python/MergeTwoList.py
@@ -0,0 +1,48 @@
+# You are given the heads of two sorted linked lists list1 and list2.
+
+# Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+# Return the head of the merged linked list.
+
+# Input: list1 = [1,2,4], list2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
+# Example 2:
+
+# Input: list1 = [], list2 = []
+# Output: []
+# Example 3:
+
+# Input: list1 = [], list2 = [0]
+# Output: [0]
+
+#Time complexity is O(n+m) as list1 and list2 = O(n)
+#Space complexity is O(1) no data stored
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        if not list1 and not list2:
+            return None
+        else:
+            if list1 and not list2:
+                return list1
+            elif not list1 and list2:
+                return list2
+        dummy = ListNode(0)
+        curr = dummy
+        while list1 and list2:
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+        if list1:
+            curr.next = list1
+        else:
+            curr.next = list2
+        return dummy.next
