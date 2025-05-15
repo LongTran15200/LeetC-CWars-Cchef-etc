04226186de2a7d54317282c2af6770a4b625d67a
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2025-05-15 00:49:46 -0500

    Create ContainsDup.py

---
+++ LeetCode/Python/NeetCode/ContainsDup.py
@@ -0,0 +1,9 @@
+# Time and space O(n)
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
